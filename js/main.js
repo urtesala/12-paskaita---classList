573ce103b7e263df8bd7bcf147ca76dfefe8ad54
urtesala <urtesaladziute@gmail.com>
2022-10-25 10:40:50 +0300

    toggle special

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const artColorInputEl = document.getElementById("artColorInput");
 const artHalfBtnEl = document.getElementById("artHalf");
 const articleEl = document.getElementById("article");
 const hideArtBtnEl = document.getElementById("hideArtBtn");
+const specialBtnEl = document.getElementById("specialBtn");
 
 function makeStyleBig(element) {
   element.style.fontWeight = "normal";
@@ -21,9 +22,16 @@ makeStyleBig(sectionTitleEl);
 makeStyleBig(autorEl);
 
 // nusittaikyti i paragrafa su tekstu, uzdedi linijos auksti 1.5 ir padidinti tarpa tarp raidziu keletu pixeliu, padaryti teksta pasvirusi i desine(italic)
-textEl.style.lineHeight = 1.5;
-textEl.style.letterSpacing = "2px";
-textEl.style.fontStyle = "italic";
+// textEl.style.lineHeight = 1.5;
+// textEl.style.letterSpacing = '2px';
+// textEl.style.fontStyle = 'italic';
+
+// MAKE SPECIAL
+specialBtnEl.addEventListener("click", () => {
+  // articleEl.className = 'specialArt'; // perraso esama
+  // articleEl.className += ' specialArt'; // nebenaudojamas
+  articleEl.classList.toggle("specialArt"); // uzdeti jei nera nuimti jei yra
+});
 
 // <input type="text" id="artColorInput" placeholder="Article spalva">
 // ivedus spalva ta spalva turi nusidazyti article fonas. be mygtuko
